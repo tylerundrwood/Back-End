40a2f12a71313646be57f76681483c708a859708
Tyler Underwood <twood9600@yahoo.com>
2023-07-16 12:23:09 -0400

    test

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,7 +5,11 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category,
+  {
+    foreignKey:'category_id',
+    onDelete: 'Cascade'
+  })
 // Categories have many Products
 
 // Products belongToMany Tags (through ProductTag)
