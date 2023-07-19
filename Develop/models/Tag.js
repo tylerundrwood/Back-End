abbeb8cef4ab8dcbf71d8b5181fbb28a0d571fb5
Tyler Underwood <twood9600@yahoo.com>
2023-07-19 17:36:54 -0400

    test

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -1,6 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 
-const sequelize = require('../Develop/config/connection.js');
+const sequelize = require('../config/connection.js');
 
 class Tag extends Model {}
 
