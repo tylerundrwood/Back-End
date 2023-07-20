0071700a823f3a4bea1a40bd46931389bc966a00
Tyler Underwood <twood9600@yahoo.com>
2023-07-20 17:39:36 -0400

    fixed error

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,11 +1,10 @@
-const Sequelize = require('sequelize');
-
 require('dotenv').config();
+const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PW,
+  process.env.DB_PASSWORD,
   {
       host: 'localhost',
       dialect: 'mysql',
